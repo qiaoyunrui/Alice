7f7e8da4765c6d75683b746a5a2dc82953ce1a19
Juhezi <juhezix@163.com>
2017-04-24 23:17:15 +0800

    update the main page

--- alicex/src/App.vue
+++ alicex/src/App.vue
@@ -1,5 +1,5 @@
-<template xmlns="">
-  <div>
+<template>
+  <div id="app">
     <mu-appbar title="Alice - 聆听这个世界">
       <mu-icon-button icon="menu" slot="left" @click="toggle(true)"></mu-icon-button>
       <mu-text-field icon="search" slot="right" hintText="搜索音乐" class="appbar-search-field"></mu-text-field>
@@ -29,6 +29,18 @@
         <mu-raised-button id="sign_in_out" label="退出登录" fullWidth primary></mu-raised-button>
       </div>
     </mu-drawer>
+    <!--中央控制模块-->
+    <div align="center" class="fill_main">
+      <div align="center" class="play-ctrl">
+        <mu-card>
+          <mu-card-title title="刚好遇见你" subTitle="李玉刚"></mu-card-title>
+          <mu-card-actions>
+            <mu-flat-button label="Action 1"></mu-flat-button>
+            <mu-flat-button label="Action 2"></mu-flat-button>
+          </mu-card-actions>
+        </mu-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,8 +50,18 @@
   import MuCardText from "../node_modules/muse-ui/src/card/cardText";
   import MuFlatButton from "../node_modules/muse-ui/src/flatButton/flatButton";
   import MuRaisedButton from "../node_modules/muse-ui/src/raisedButton/raisedButton";
+  import MuCard from "../node_modules/muse-ui/src/card/card";
+  import MuCardHeader from "../node_modules/muse-ui/src/card/cardHeader";
+  import MuAvatar from "../node_modules/muse-ui/src/avatar/avatar";
+  import MuCardMedia from "../node_modules/muse-ui/src/card/cardMedia";
+  import MuCardTitle from "../node_modules/muse-ui/src/card/cardTitle";
   export default {
     components: {
+      MuCardTitle,
+      MuCardMedia,
+      MuAvatar,
+      MuCardHeader,
+      MuCard,
       MuRaisedButton,
       MuFlatButton,
       MuCardText,
@@ -87,7 +109,15 @@
     }
   }
 </style>
+
 <style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
   .container {
     padding: 12px;
@@ -96,4 +126,15 @@
   .pickname-field {
     color: #f0f;
   }
+
+  .play-ctrl {
+    width: 500px;
+  }
+
+  .fill_main {
+    background: #00f;
+    width: 100%;
+  }
+
+
 </style>
